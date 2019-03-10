a1cf0dc5639728b238a4c6eb82c0ba3c7e0cdb11
Whitemoon <daniel@wydler.eu>
2019-03-10 09:54:25 +0100

    fix description

--- ActiveDirectory/copy-members-of-adgroup.ps1
+++ ActiveDirectory/copy-members-of-adgroup.ps1
@@ -2,5 +2,5 @@
 [string] $strSourceGroup = "NameDerQuellGrueppe"
 [string] $strDestinationGroup = "NameDerZielGruppe"
 
-# Main process
+# Main programm
 Get-ADGroupMember $strSourceGroup | Select SAMAccountName | ForEach { Add-ADGroupMember $strDestinationGroup -Members $_.SAMAccountName }
