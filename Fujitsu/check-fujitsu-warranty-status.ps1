dc193578a245a20f4aa96f3a2d1a2a75488cd0b8
Whitemoon <daniel@wydler.eu>
2019-03-10 10:35:24 +0100

    remove serial from demo pc

--- Fujitsu/check-fujitsu-warranty-status.ps1
+++ Fujitsu/check-fujitsu-warranty-status.ps1
@@ -3,7 +3,7 @@
 Fujitsu Garantie / Service Status ueberpruefen
 ===========================================================================
 Autor:     Daniel Wydler
-Umgebung:  Windows 10 (1607), Powershell 5.1
+Umgebung:  Windows 10 (1703), Powershell 5.1.15063.502
 #>
 
 param(
@@ -36,7 +36,8 @@ Function GetWarrantyInfo ([string] $strLocSerialNumber) {
     
     # Garantiedaten auf der Fujitsu-Webseite abfragen
     try {
-        [Microsoft.PowerShell.Commands.WebResponseObject] $wroLocHtml = Invoke-WebRequest "http://support.ts.fujitsu.com/Warranty/WarrantyStatus.asp?lng=DE&IDNR=$strLocSerialNumber"
+        $wroSearchHtml= Invoke-WebRequest "http://support.ts.fujitsu.com/include/Suchfunktion.asp?lng=DE&GotoURL=IndexWarranty&Search=$strLocSerialNumber" -SessionVariable session
+        $wroLocHtml = Invoke-WebRequest "http://support.ts.fujitsu.com/Warranty/WarrantyStatus.asp?lng=DE&IDNR=$strLocSerialNumber&HardwareGUID=428EF436-8E40-49C1-9C07-F10F56903BF3&Version=3.51" -WebSession $session
     }
     catch {
         Write-Host $($_.Exception.Message) -ForegroundColor Red
@@ -45,7 +46,7 @@ Function GetWarrantyInfo ([string] $strLocSerialNumber) {
 
 
     # Filtern der notwendigen Informationen aus der HTML Quellcode
-    return ($wroLocHtml.ParsedHtml.getElementsByTagName("td") | Where { ($_.className -eq ‘contenttext’)-or ($_.className -eq ‘alink’) }).outertext    
+    return ($wroLocHtml.ParsedHtml.getElementsByTagName("td") | Where { ($_.className -eq ‘contenttext’) }).outertext    
 }
 
 Function SetWarrantyEnd ([array] $aLocProductInfo) {
@@ -56,17 +57,17 @@ Function SetWarrantyEnd ([array] $aLocProductInfo) {
     #if($aProductInfo[2] -match "[A-Za-zä\s]{0,18}\d\s[a-z]{0,3}\s\d\s[A-Za-z\s\-]{0,21}") {
     if($aLocProductInfo[2] -match "[A-Za-zä\s]+\d[A-Za-zä\s]+\d[A-Za-zä\s\-]+") {
         $htMatches = $aLocProductInfo[2] -split "(\d+)"
-        return (Get-Date $aLocProductInfo[6]).AddYears($htMatches[3])
+        return (Get-Date $aLocProductInfo[7]).AddYears($htMatches[3])
     }
     # 5 Jahre Vor-Ort Service, 9x5, nächster Arbeitstag Antrittszeit, gilt im Land des Erwerbs
     #elseif ($aProductInfo[2] -match "(\d\s[A-Za-z\s\-]{0,21}\,\s\d[x]\d)") {
     elseif ($aLocProductInfo[2] -match "\d[a-zA-Z\s\-]+\,\s\dx\d") {
         $htMatches = $aLocProductInfo[2] -split "([\sA-Za-z\-\,ä]+)(\d[x]\d)"
-        return (Get-Date $aLocProductInfo[6]).AddYears($htMatches[0])
+        return (Get-Date $aLocProductInfo[7]).AddYears($htMatches[0])
     }
     else {
-        Write-Host "Unbekannter Garantiestatus!" -ForegroundColor Red
-        break;
+        #Write-Host "Unbekannter Garantiestatus!" -ForegroundColor Red
+        return (Get-Date $aLocProductInfo[7])
     }
 
 }
@@ -75,15 +76,19 @@ Function OutputInformations ([array] $aLocProductInfo, [datetime] $dtLocServiceE
 
     Write-Host "Produktname:`t`tFujitsu $($aLocProductInfo[0].Trim())"
     Write-Host "Bestellnummer:`t`t$($aLocProductInfo[3].Trim())"
-    Write-Host "Service Code:`t`t$($aLocProductInfo[5].Trim())"
-    Write-Host "Service Start:`t`t$($aLocProductInfo[6].Trim())"
+    Write-Host "Service Code:`t`t$($aLocProductInfo[6].Trim())"
+    Write-Host "Service Start:`t`t$($aLocProductInfo[7].Trim())"
     Write-Host "Service Ende:`t`t" -NoNewline
-        if ($dtLocServiceEnd -gt (get-date)) { write-host "$($dtLocServiceEnd.ToString("dd.MM.yyyy"))" -ForegroundColor Green }
+        if ($aLocProductInfo[1] -eq "Der Service für das Produkt ist abgelaufen") { Write-Host "" }
+        elseif ($dtLocServiceEnd -gt (get-date)) { write-host "$($dtLocServiceEnd.ToString("dd.MM.yyyy"))" -ForegroundColor Green }
         elseif ($dtLocServiceEnd -lt (get-date)) { write-host "$($dtLocServiceEnd.ToString("dd.MM.yyyy"))" -ForegroundColor Red }
+        
 
     Write-Host "Service Status:`t`t$($aLocProductInfo[1].Trim())"
     Write-Host "Garantie Gruppe: `t$($aLocProductInfo[4].Trim())"
-    Write-Host "Garantie Art:`t`t$($aLocProductInfo[2].Trim())"
+    Write-Host "Garantie Art:`t`t" -NoNewline
+        if ($aLocProductInfo[2] -ne $null) { $aLocProductInfo[2].Trim() }
+        else { Write-host "---" -ForegroundColor Red }
 }
 
 
@@ -98,14 +103,14 @@ Write-Host @"
 ------------------------------------------------------------------------------------------------------------------------
 
                                         Fujitsu Garantie / Service Status ueberpruefen
-                                                        Version: 0.2
+                                                        Version: 0.3
 
 ------------------------------------------------------------------------------------------------------------------------
 "@
 
 # Abfrage, ob Seriennummer übergeben worden ist. Falls nicht, kann diese manuell eingegeben werden.
 if( -not($SerialNumber) ) {
-    $SerialNumber = Read-Host -Prompt "Seriennummer des Geraets eingeben (z.B. YLLC001597 oder YM5G017873 )"
+    $SerialNumber = Read-Host -Prompt "Seriennummer des Geraets eingeben (z.B. YLLC001597 oder YM5G017873)"
     [bool] $cli = $false
 }
 else {
