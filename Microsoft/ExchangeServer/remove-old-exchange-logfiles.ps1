94effdd28eea89d49ae6f22d8650160b76ed7f8c
Whitemoon <daniel@wydler.eu>
2019-03-16 12:34:17 +0100

    Migrate to Github

---
+++ Microsoft/ExchangeServer/remove-old-exchange-logfiles.ps1
@@ -0,0 +1,38 @@
+Clear-Host
+Set-Executionpolicy RemoteSigned
+
+
+$days=14
+
+$IISLogPath="C:\inetpub\logs\LogFiles\"
+$ExchangeLoggingPath="C:\Program Files\Microsoft\Exchange Server\V15\Logging\"
+$ETLTracesPath="C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\ETLTraces\"
+$LoggingPath="C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\Logs\"
+
+
+Function CleanLogfiles($TargetFolder)
+{
+  write-host -debug -ForegroundColor Yellow -BackgroundColor Cyan $TargetFolder
+
+    if (Test-Path $TargetFolder) {
+        $Now = Get-Date
+        $LastWrite = $Now.AddDays(-$days)
+    #   $Files = Get-ChildItem $TargetFolder -Include *.log,*.blg, *.etl -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+        $Files = Get-ChildItem $TargetFolder -Recurse | Where-Object {$_.Name -like "*.log" -or $_.Name -like "*.blg" -or $_.Name -like "*.etl"}  | where {$_.lastWriteTime -le "$lastwrite"} | Select-Object FullName  
+        foreach ($File in $Files)
+            {
+               $FullFileName = $File.FullName  
+               Write-Host "Deleting file $FullFileName" -ForegroundColor "yellow"; 
+               Remove-Item $FullFileName -ErrorAction SilentlyContinue | out-null
+            }
+       }
+Else {
+    Write-Host "The folder $TargetFolder doesn't exist! Check the folder path!" -ForegroundColor "red"
+    }
+}
+
+
+CleanLogfiles($IISLogPath)
+CleanLogfiles($ExchangeLoggingPath)
+CleanLogfiles($ETLTracesPath)
+CleanLogfiles($LoggingPath)
