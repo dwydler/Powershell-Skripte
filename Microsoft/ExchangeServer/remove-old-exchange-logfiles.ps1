022592c5f0b2c876c95c77c7ecff1b47193a7ca3
Whitemoon <daniel@wydler.eu>
2019-03-16 12:39:26 +0100

    remove old code

--- Microsoft/ExchangeServer/remove-old-exchange-logfiles.ps1
+++ Microsoft/ExchangeServer/remove-old-exchange-logfiles.ps1
@@ -17,7 +17,6 @@ Function CleanLogfiles($TargetFolder)
     if (Test-Path $TargetFolder) {
         $Now = Get-Date
         $LastWrite = $Now.AddDays(-$days)
-    #   $Files = Get-ChildItem $TargetFolder -Include *.log,*.blg, *.etl -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
         $Files = Get-ChildItem $TargetFolder -Recurse | Where-Object {$_.Name -like "*.log" -or $_.Name -like "*.blg" -or $_.Name -like "*.etl"}  | where {$_.lastWriteTime -le "$lastwrite"} | Select-Object FullName  
         foreach ($File in $Files)
             {
