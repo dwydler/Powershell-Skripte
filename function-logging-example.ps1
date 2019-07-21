13447dcd0cc910801f1ca6bb811621bd3ae4709c
Blackmoon <dwydler@users.noreply.github.com>
2019-07-21 13:13:44 +0200

    Update function-logging-example.ps1
    - Added a Header to every logfile.
    - Remove all pipes to stored message in the logfile.
    - Added parameter (-Eventlog) to write messages into the event log (optional)
    - Added parameter (-Absatz) to get a empty line in the log
    - Set a default value to $LogStatus
    - Rename variable $logLevel to $LogStatus
    - Rewrite the query of $LogStatus
    - Added different log messages with different parameter.

--- function-logging-example.ps1
+++ function-logging-example.ps1
@@ -1,34 +1,94 @@
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
-
 [string] $strLogfilePath = "C:\Temp"
-[string] $strLogfileDate = Get-Date -Format "yyyy-MM-dd_HH-mm"
-[string] $strLogfileName = ("Log_" + $strLogfileDate + "_"+ $env:USERDOMAIN + "_" + $env:USERNAME + ".log")
+[string] $strLogfileDate = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+[string] $strLogfileName = ("Log_" + $strLogfileDate + ".log")
 [string] $strLogfile = $strLogfilePath + "\" + $strLogfileName
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
 function Write-Log {
+    [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(
+            Mandatory=$true,
+            Position=0)]
+        [ValidateNotNullOrEmpty()]
         [string] $LogText = "",
 
-        [Parameter(Mandatory=$true)]
-        [ValidateRange(0,3)]
-        [int] $LogLevel=0
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Info','Success','Warning','Error')]
+        [string] $LogStatus= "Info",
+
+        [Parameter(Mandatory=$false)]
+        [switch] $Absatz,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $EventLog
     )
 
 	[string] $strLogdate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    [string] $strTextColor = "White"
+    [string] $strLogFileAbsatz = ""
+    [string] $strLogFileHeader = ""
 
-    [array] $aDebugLevel = @("INFO   ", "WARNING", "ERROR  ", "SUCCESS")
-    [array] $aDebugTextColor = @("White", "Yellow", "Red", "Green")
+    # Add a header to logfile, if the logfile not exist
+    If ( -not (Test-Path $strLogfile) ) {
+        $strLogFileHeader = "$("#" * 75)`n"
+        $strLogFileHeader += "{0,-21} {1,0}" -f "# Skript:", "$($MyInvocation.ScriptName)`n"
+        $strLogFileHeader += "{0,-21} {1,0}" -f "# Startzeit:", "$(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        $strLogFileHeader += "{0,-21} {1,0}" -f "# Startzeit:", "$(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        $strLogFileHeader += "{0,-21} {1,0}" -f "# Ausführendes Konto:", "$([Security.Principal.WindowsIdentity]::GetCurrent().Name)`n"
+        $strLogFileHeader += "{0,-21} {1,0}" -f "# Computername:", "$env:COMPUTERNAME`n"
+        $strLogFileHeader += "$("#" * 75)`n"
 
-    $LogText = "$strLogdate - $($aDebugLevel[$LogLevel]) - $LogText"
+        Write-Host $strLogFileHeader
+        Add-Content -Path $strLogfile -Value $strLogFileHeader -Encoding UTF8
+    }
+   
 
-    Write-Host $LogText -ForegroundColor $aDebugTextColor[$LogLevel]
-    "$LogText" | Out-File -FilePath $strLogfile -Append
+    switch($LogStatus) {
+        Info {
+            $strTextColor = "White"
+        }
+        Success {
+            $strTextColor = "Green"
+        }
+        Warning {
+            $strTextColor = "Yellow"
+        }
+        Error {
+            $strTextColor = "Red"
+        }
+    }
+
+    # Add an Absatz if the parameter is True
+    if($Absatz) {
+        [string] $strLogFileAbsatz = "`r`n"
+    }
+
+    #Format the text output
+    $LogText = "{0,-20} - {1,-7} - {2,0}" -f "$strLogdate", "$LogStatus", "$LogText $strLogFileAbsatz"
+
+    # Write output to powershell console
+    Write-Host $LogText -ForegroundColor $strTextColor
+
+    # Write output to logfile
+    Add-Content -Path $strLogfile -Value $LogText -Encoding UTF8
+
+    # Add Logfile to local Eventlog of the operating system 
+    if($EventLog) {
+        Write-EventLog -LogName 'Windows PowerShell' -Source "Powershell" -EventId 0 -Category 0 -EntryType $LogStatus -Message $LogText
+    }
 
 }
 
+
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
-Write-Log -LogText "Das Verzeichnis $strVmWareBackupPath\$strVmWareBackupFolder wird angelegt..." -LogLevel 0
+Write-Log -LogText "Das ist ein Test." -LogStatus Info
+Write-Log -LogText "Das ist ein Test." -LogStatus Success
+Write-Log -LogText "Das ist ein Test." -LogStatus Warning
+Write-Log -LogText "Das ist ein Test." -LogStatus Error -Absatz
+Write-Log -LogText "Das ist ein Test."
+#Write-Log -LogText ""
