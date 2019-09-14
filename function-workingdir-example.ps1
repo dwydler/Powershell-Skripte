6553d12499a0b4133d599605566b73b08399f19e
Blackmoon <dwydler@users.noreply.github.com>
2019-09-14 12:52:56 +0200

    Migrate to Github

---
+++ function-workingdir-example.ps1
@@ -0,0 +1,26 @@
+function WorkingDir {
+    param (
+         [parameter(
+            Mandatory=$false,
+            Position=0
+          )]
+        [switch] $Debugging
+    )
+
+    # Splittet aus dem vollständigen Dateipfad den Verzeichnispfad heraus
+    # Beispiel: D:\Daniel\Temp\Unbenannt2.ps1 -> D:\Daniel\Temp
+    [string] $strWorkingdir = Split-Path $MyInvocation.PSCommandPath -Parent
+
+    # Wenn Variable wahr ist, gebe Text aus.
+    if ($Debugging) {
+        Write-Host "[DEBUG] PS $strWorkingdir`>" -ForegroundColor Gray
+    }
+
+    # In das Verzeichnis wechseln
+    cd $strWorkingdir
+}
+
+Clear-Host
+
+WorkingDir
+WorkingDir -Debugging
