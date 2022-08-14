b79b5b40f6f6522f4eb519161da7a6f149c9bf92
Blackmoon <dwydler@users.noreply.github.com>
2022-08-14 20:42:56 +0200

    Update paessler-prtg_vmware-horizion-user-sessions-stats.ps1
    Removed Variable Type from $Value

--- Paessler/PRTG/paessler-prtg_vmware-horizion-user-sessions-stats.ps1
+++ Paessler/PRTG/paessler-prtg_vmware-horizion-user-sessions-stats.ps1
@@ -162,7 +162,7 @@ $QueryResult = Invoke-Command -Computername $PrtgDevice -ArgumentList $PrtgDevic
         [string]$Channel,
     
         [Parameter(mandatory=$True,Position=1)]
-        [string]$Value,
+        $Value,
     
         [Parameter(mandatory=$True,Position=2)]
         [string]$Unit,
