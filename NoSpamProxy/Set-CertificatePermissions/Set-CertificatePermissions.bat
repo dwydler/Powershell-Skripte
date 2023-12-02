2a4fb46edd36ee10521189f721da6a59c27f9d62
Daniel Wydler <daniel@wydler.eu>
2023-12-02 15:15:12 +0100

    Fixed directory change command

--- NoSpamProxy/Set-CertificatePermissions/Set-CertificatePermissions.bat
+++ NoSpamProxy/Set-CertificatePermissions/Set-CertificatePermissions.bat
@@ -4,7 +4,7 @@ rem Aktuelles Verzeichnis des Skripts in Variable schreiben
 SET CURRENTDIR="%~dp0"
 
 rem In das Verzeichnis wechseln
-cd "%CURRENTDIR%"
+cd /D "%CURRENTDIR%"
 
 rem PowerShell Skript starten
 "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -File "Set-CertificatePermissions.ps1"
